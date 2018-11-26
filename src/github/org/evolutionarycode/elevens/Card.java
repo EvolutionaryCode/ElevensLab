36a751f6997885e59c1984248024d720e480069f
Austin Grandpre <ausgran@icloud.com>
2018-11-26 17:53:18 -0500

    Almost done with Activity 2 #1.a

--- src/github/org/evolutionarycode/elevens/Card.java
+++ src/github/org/evolutionarycode/elevens/Card.java
@@ -89,6 +89,6 @@ public class Card {
      */
     @Override
     public String toString() {
-        return rank + " of " + suit + " (point value = " + pointValue + ")";
+        return rank + " of " + suit + " (point value = " + pointValue + ")  ";
     }
 }
