dee7ed39bd082d11c3c4a27cf05b111d22b0881d
Austin Grandpre <ausgran@icloud.com>
2018-11-30 09:49:56 -0500

    Cleaned up Card Testing!

--- src/github/org/evolutionarycode/elevens/CardTester.java
+++ src/github/org/evolutionarycode/elevens/CardTester.java
@@ -6,14 +6,18 @@ public class CardTester {
          *	@param args is not used.
          */
         public static void main(String[] args) {
-            Card card1 = new Card("Heart", "6", 6);
-            Card card2 = new Card("Heart", "6", 6);
-            Card card3 = new Card("Heart", "Jack", 11);
+            Card card1 = new Card("6", "Hearts", 6);
+            Card card2 = new Card("6", "Hearts", 6);
+            Card card3 = new Card("Jack", "Hearts", 11);
 
+            System.out.println("-------[This the the practice cards values!]-------");
             System.out.println(card1.toString());
             System.out.println(card2.toString());
             System.out.println(card3.toString());
 
+            System.out.println(); // #BlankLine
+
+            System.out.println("-------[What Card Match?]-------");
             System.out.println("Card 1 and Card 2 should match: " + card2.matches(card1));
             System.out.println("Card 2 and Card 3 should not match: " + !card2.matches(card3));
         }
